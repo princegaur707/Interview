df3a9ffcaf03ba08901d67bc98ddfd3115b5c1f6
Prince Gaur <48658242+proxy707@users.noreply.github.com>
2020-05-16 23:03:06 +0530

    Update ksubsets.py

--- Dynamic Programming/ksubsets.py
+++ Dynamic Programming/ksubsets.py
@@ -8,3 +8,16 @@ class Solution:
             return False
         nums.sort(reverse=True)
         target = [sm/k]*k
+    
+        def dfs(pos):
+            if pos == len(nums):
+                return True
+            for i in range(k):
+                if target[i] >= nums[pos]:
+                    target[i] -= nums[pos]
+                    if dfs(pos+1):
+                        return True
+                    target[i] += nums[pos] 
+            return False
+        return  dfs(0)
+                
