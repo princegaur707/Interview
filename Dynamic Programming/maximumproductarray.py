ed623b50462e74dbfa35f7e8398275fb85991343
Prince Gaur <48658242+proxy707@users.noreply.github.com>
2020-05-16 23:04:08 +0530

    Update maximumproductarray.py

--- Dynamic Programming/maximumproductarray.py
+++ Dynamic Programming/maximumproductarray.py
@@ -5,3 +5,6 @@ class Solution:
         for i in range(1,len(A)):
             if A[i-1] != 0:
                 A[i]*=A[i-1]
+            if B[i-1] != 0:
+                B[i]*=B[i-1]
+        return max(A+B)
