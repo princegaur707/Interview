3f3451a980faee2a9bb80ccd3882955f54796eee
Prince Gaur <48658242+proxy707@users.noreply.github.com>
2020-05-18 23:08:51 +0530

    Create genprime.py

---
+++ Number Theory/genprime.py
@@ -0,0 +1,10 @@
+def genprimes(n):
+    primes[True]*(n+1)
+    primes[0]=primes[1]=False
+    for i in range(2,len(primes)):
+        if primes[i]:
+            for j in range(
+t = int(input())
+while t:
+    print(genprimes(n))
+    t=t-1
