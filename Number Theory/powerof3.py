be27c13c160e5f4093c61f6f19815a0bb06b9d3d
Prince Gaur <48658242+proxy707@users.noreply.github.com>
2020-05-20 23:01:18 +0530

    Update powerof3.py

--- Number Theory/powerof3.py
+++ Number Theory/powerof3.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n<=0:
+            return False
+        from math import floor,ceil,sqrt
+        x = log10(n)/log10(3)
+        
