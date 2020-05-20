ccb4a7b9b736f5831c90028da79d777989c26eac
Prince Gaur <48658242+proxy707@users.noreply.github.com>
2020-05-20 23:06:31 +0530

    Update leapyear.py

--- Number Theory/leapyear.py
+++ Number Theory/leapyear.py
@@ -1,1 +1,13 @@
+def isleapyear(l):
+	flag1 = l%4 == 0 and l%100 != 0
+	flag2 = l%400 == 0
+	if flag1 or flag2:
+		return True
+	else:
+		return False
 
+t = int(input())
+while t:
+	leapyear = int(input())
+	print(isleapyear(leapyear))
+	t=t-1
