ba3a960a22ed6c0181014ceb22ac1ea2add352c4
Prince Gaur <48658242+proxy707@users.noreply.github.com>
2020-05-21 23:31:56 +0530

    Update pascaltriangle.py

--- Number Theory/pascaltriangle.py
+++ Number Theory/pascaltriangle.py
@@ -8,3 +8,8 @@ class Solution:
         for i in range(3,n+1):
             newrow = [0]*(i)
             newrow[0] = newrow[-1] = 1
+            for j in range(1,len(newrow)-1):
+                newrow[j] = res[-1][j]+res[-1][j-1]
+            res.append(newrow)
+    
+        return res[1:]
