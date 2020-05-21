6e375551b4c02d2546c418375e6af37e6b871534
Prince Gaur <48658242+proxy707@users.noreply.github.com>
2020-05-21 23:31:40 +0530

    Update pascaltriangle.py

--- Number Theory/pascaltriangle.py
+++ Number Theory/pascaltriangle.py
@@ -4,3 +4,7 @@ class Solution:
             return []
         res = [[0],[1],[1,1]]
         if n <= 2:
+            return res[1:n+1]
+        for i in range(3,n+1):
+            newrow = [0]*(i)
+            newrow[0] = newrow[-1] = 1
