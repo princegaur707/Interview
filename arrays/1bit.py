852f1ad06b6912483979a921f5b9debd1409993c
proxy707 <princegaur1998@gmail.com>
2020-08-30 05:29:52 +0530

    file changed

--- arrays/1bit.py
+++ arrays/1bit.py
@@ -1,4 +1,15 @@
 https://leetcode.com/problems/number-of-1-bits/
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         return bin(n).count('1')
+
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        return bin(n).count('1')
+    def countBits(self, num: int) -> List[int]:
+        bits=[0]*(num+1)
+        for i in range(num+1):
+            bits[i]=bits[i//2]+i%2
+        return bits
+
+Solution sol 
+n=input("Enter: ")
+print(sol.countBits(n))
--- arrays/sumoftwo.py
+++ arrays/sumoftwo.py
@@ -1,27 +1,10 @@
-def Add(x, y): 
-    while (y != 0): 
-	    # carry now contains common 
-		# set bits of x and y 
-        carry = x & y 
-        print("carry:",carry)
-
-		# Sum of bits of x and y where at 
-		# least one of the bits is not set 
-        x = x ^ y 
-        print("x:",x)
-
-		# Carry is shifted by one so that 
-		# adding it to x gives the required sum
-        y = carry << 1
-        print("y:",y)
-    return x
-print(Add(15, 15)) 
-
-# #Recursive solution
-# int Add(int x, int y) 
-# { 
-# 	if (y == 0) 
-# 		return x; 
-# 	else
-# 		return Add( x ^ y, (x & y) << 1); 
-#} 
+#JAVA code 
+#Same can't be done with python as it deals with negative numbers too bitwise
+class Solution {
+    public int getSum(int a, int b) {
+        if(b == 0){
+            return a;
+        }
+        return getSum(a^b,(a&b) << 1);
+    }
+}
