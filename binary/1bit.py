ee74416c393006b48dda8bcc9c83c5fe83cf1734
proxy707 <princegaur1998@gmail.com>
2020-08-30 17:54:08 +0530

    file changed

--- binary/1bit.py
+++ binary/1bit.py
@@ -1,15 +1,9 @@
-https://leetcode.com/problems/number-of-1-bits/
-# class Solution:
-#     def hammingWeight(self, n: int) -> int:
-#         return bin(n).count('1')
-
 class Solution:
-    def countBits(self, num: int) -> List[int]:
-        bits=[0]*(num+1)
-        for i in range(num+1):
-            bits[i]=bits[i//2]+i%2
-        return bits
-
-Solution sol 
-n=input("Enter: ")
-print(sol.countBits(n))
+    def hammingWeight(self, n: int) -> int:
+        ones=0
+        while n:
+            if n%2==1:
+                ones+=1
+            n//=2
+        return ones
+        
